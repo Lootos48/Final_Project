6442ba991af5a0446fb91e30cea334f7c432c1b6
Lootos48 <danil.sribny@gmail.com>
2022-01-24 10:22:27 +0200

    Fixed data in DB

--- CardFile.BLL/DTO/UserAuthInfoDTO.cs
+++ CardFile.BLL/DTO/UserAuthInfoDTO.cs
@@ -12,10 +12,8 @@ namespace CardFile.BLL.DTO
     /// </summary>
     public class UserAuthInfoDTO
     {
-        [StringLength(40, MinimumLength = 3, ErrorMessage = "User name size must be between 3 and 40 characters")]
         public string Username { get; set; }
 
-        [StringLength(100, ErrorMessage = "User name size must be between 5 and 100 characters")]
         public string Password { get; set; }
     }
 }
